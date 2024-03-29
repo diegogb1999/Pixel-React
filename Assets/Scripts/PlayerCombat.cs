b87a2d3aedf0d21980fd17029358f8b9f568b9c4
diegogb1999 <diegogb1999@gmail.com>
2024-03-29 14:03:32 +0100

    Hitbox ataque principal, e skill

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,20 @@ public class PlayerCombat : MonoBehaviour
 {
 
     public Animator animator;
+    public Transform attackPointBasicAttack;
+    public Transform attackPointEskill;
+    public LayerMask enemyLayers;
+
+    public float attackRangeBasicAttack; //0.5f
+    public float attackRateBasicAttack; //1.2f
+
+    float nextAttackTime = 0f;
+
+    public float attackRangeEskillX; //2.06f
+    public float attackRangeEskillY; //0.67f
+    public float attackRateEskill; //0.2f
+
+    float nextAttackTimeEskill = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,22 +31,82 @@ public class PlayerCombat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Attack();
-        Attack2();
+        BasicAttack();
+        SecondaryAttack();
+        Eskill();
 
     }
-    void Attack()
+    void BasicAttack()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            animator.SetTrigger("attack");
-        }
+            if (Input.GetKeyDown(KeyCode.Space) && Time.time >= nextAttackTime)
+            {
+                animator.SetTrigger("basicAttack");
+                Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPointBasicAttack.position, attackRangeBasicAttack, enemyLayers);
+
+                foreach (Collider2D enemy in hitEnemies)
+                {
+                    Debug.Log("hit " + enemy.name);
+                }
+
+                nextAttackTime = Time.time + 1f / attackRateBasicAttack;
+            }
     }
-    void Attack2()
+
+    void OnDrawGizmos()
+    {
+
+        if (attackPointBasicAttack != null)
+        {
+
+            Gizmos.DrawWireSphere(attackPointBasicAttack.position, attackRangeBasicAttack);
+        }
+        
+        if (attackPointEskill != null)
+        {
+            Vector2 hitBoxSize = new Vector2(attackRangeEskillX, attackRangeEskillY); 
+
+ 
+            Vector2 hitBoxPosition = new Vector2(attackPointEskill.position.x, attackPointEskill.position.y);
+
+            Gizmos.DrawWireCube(hitBoxPosition, hitBoxSize);
+        }
+
+    }
+
+    void SecondaryAttack()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            animator.SetTrigger("attack2");
+            animator.SetTrigger("secondaryAttack");
+        }
+    }
+
+    void Eskill()
+    {
+        /*if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.SetTrigger("eSkill");
+        }*/
+
+        if (Input.GetKeyDown(KeyCode.E) && Time.time >= nextAttackTimeEskill)
+        {
+            animator.SetTrigger("eSkill");
+
+            // Especifica el tamaño de la hitbox. Aumenta el primer valor para hacerla más ancha.
+            // Ajusta el tamaño para hacer la hitbox más larga.
+            Vector2 hitBoxSize = new Vector2(attackRangeEskillX, attackRangeEskillY);
+
+
+            Vector2 hitBoxPosition = new Vector2(attackPointEskill.position.x, attackPointEskill.position.y);
+
+            Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(hitBoxPosition, hitBoxSize, 0, enemyLayers);
+
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                Debug.Log("hit E " + enemy.name);
+            }
+
+            nextAttackTimeEskill = Time.time + 1f / attackRateEskill;
         }
     }
 }
